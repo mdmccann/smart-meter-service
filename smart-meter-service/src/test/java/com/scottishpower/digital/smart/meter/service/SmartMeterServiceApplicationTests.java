92c85dd3a1de670ec76a01fd9bfb60e61c70197d
Marty <martymccann@live.co.uk>
2020-02-02 19:25:57 +0000

    Setting up project structure

---
+++ smart-meter-service/src/test/java/com/scottishpower/digital/smart/meter/service/SmartMeterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scottishpower.digital.smart.meter.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMeterServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
